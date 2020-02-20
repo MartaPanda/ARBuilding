3fac23fdb95b77832064d0a0835356ed0a9addd4
Marta Salaban <marta.salaban@vakoms.com.ua>
2020-02-20 16:12:14 +0200

    Added canvas and scroll menu

---
+++ Assets/Scripts/SnapScroll.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class SnapScroll : MonoBehaviour
+{
+    public GameObject itemPrefab;
+    [Range (1, 50)]
+    public int itemCount;
+    private GameObject item;
+
+    [NonSerialized]
+    public List<GameObject> items = new List<GameObject>();
+
+    public void Start()
+    {
+        for (int i = 0; i < itemCount; i++)
+        {
+            item = Instantiate(itemPrefab, transform, false);
+            if (items.Count == 0)
+            {
+                print("++");
+                items.Add(item);
+
+                continue;
+            }
+            //item.transform.position = new Vector3(0, 0, 0);
+            else
+            {
+                var itemTransform = items.ElementAt(items.Count - 1).transform.localPosition;
+                print(itemTransform);
+                item.transform.localPosition = new Vector3(itemTransform.x + 10, itemTransform.y, 0);
+                print($"item.transform.position {item.transform.position}");
+            }
+            items.Add(item);
+        }
+    }
+
+
+}
